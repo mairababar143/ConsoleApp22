0cd44abd63852f25fd394daca6f236bab4c48d3e
mairababar143 <mairababar143@gmail.com>
2019-11-01 10:30:15 +0500

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            int option = 0, semester = 0;
+            string studentID = " ", name = " ", department = " ", university = " ", attendance = " " ;
+            float cgpa = 0;
+            Console.Write("Main Menu\n\n1. Create New Student\n2. Search Student\n3. Delete Student Record\n4. List top 03 of class\n5. Mark student attendance\n6. View attendance\n\nOption: ");
+            option = Convert.ToInt32(Console.ReadLine());
+            student.GetInfo();
+
+            switch(option)
+            {
+                case 1:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Create New Student\n");
+                        Console.Write("Enter the following information for a new student:\n\nStudent ID: ");
+                        studentID = Console.ReadLine();
+                        Console.Write("Name: ");
+                        name = Console.ReadLine();
+                        Console.Write("Attendance: ");
+                        attendance = Console.ReadLine();
+                        Console.Write("CGPA: ");
+                        cgpa = (float)Convert.ToDecimal(Console.ReadLine());
+                        Console.Write("Semester: ");
+                        semester = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Department: ");
+                        department = Console.ReadLine();
+                        Console.Write("University: ");
+                        university = Console.ReadLine();
+                        student.NewStudent(name, attendance, cgpa, semester, department, university, studentID);
+                        break;
+                    }
+                case 2:
+                    {
+                        break;
+                    }
+                case 3:
+                    {
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+                case 5:
+                    {
+                        break;
+                    }
+                case 6:
+                    {
+                        break;
+                    }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp22/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp22
+{
+    class Student
+    {
+        float cgpa;
+        int semester;
+        string name, department, university, attendance, studentID;
+        List<Student> listObject;
+
+        public Student()
+        {
+            cgpa = 0;
+            semester = 0;
+            name = department = university = attendance = studentID = " ";
+            listObject = new List<Student>();
+        }
+
+        public void GetInfo()
+        {
+            string line = " ";
+            System.IO.StringReader file = new System.IO.StringReader(@"D:\Desktop\Student.txt");
+            while((line = file.ReadLine()) != null)
+            {
+                studentID = line;
+                name = file.ReadLine();
+                attendance = file.ReadLine();
+                cgpa = (float)Convert.ToDecimal(file.ReadLine());
+                semester = Convert.ToInt32(file.ReadLine());
+                department = file.ReadLine();
+                university = file.ReadLine();
+                listObject.Add(new Student() { studentID = this.studentID, name = this.name, attendance = this.attendance, cgpa = this.cgpa, semester = this.semester, department = this.department, university = this.university });
+            }
+            file.Close();
+        }
+
+        public void NewStudent(string n, string a, float c, int s, string d, string u, string id)
+        {
+            listObject.Add(new Student() { studentID = id, name = n, attendance = a, cgpa = c, semester = s, department = d, university = u });
+            StreamWriter write;
+            using (write = new StreamWriter(@"D:\Desktop\Student.txt", true))
+            {
+                write.WriteLine(id+"\n"+n+"\n"+a+"\n"+c+"\n"+s+"\n"+d+"\n"+u);
+            }
+            Console.WriteLine("List Name: {0}", listObject[0].name);
+        }
+        
+
+    }
+}
